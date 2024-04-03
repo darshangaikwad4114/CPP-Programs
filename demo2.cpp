c1762c79e3ce157060db5801b1d51adddbf178d9
darshan <darshangaikwad4114@gmail.com>
2024-04-03 23:57:37 +0530

    upload programs

---
+++ demo2.cpp
@@ -0,0 +1,14 @@
+// 1. C++ Program to Print Number Entered by User
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    cout << "You entered " << number;
+    return 0;
+}
